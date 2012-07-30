d524a2c63befdbeab97afbd131b99320bf49cb30
Kenneth Peters <Me@Mine.com>
2012-07-30 15:18:24 +0000

    changes:

--- part1.sh
+++ part1.sh
@@ -1,15 +1,21 @@
 #!usr/ruby/bin/env ruby
 
+def count_words (string)
+  output = string.gsub(/\b/, '')
+  puts "This is the word broken up by words" + output
+end
+
 def palindrome? (string)
   output2 = string.gsub(/\W/, '').downcase
   if output2.reverse == output2 # Checks if the string is the same reversed
-    puts "#{ string } is a palindrome."
+    #puts "#{string} is a palindrome."
+    output2
   else
-   puts "#{string} is not a palindrome."
+    #puts "#{string} is not a palindrome."
+    output2
   end
 end
 
-
 print "Please tell me the phrase you would like to check: "
-name = gets.chomp
-palindrome? name.to_s
+palindrome? name = gets.chomp
+#count_words name
